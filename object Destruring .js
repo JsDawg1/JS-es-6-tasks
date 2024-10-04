a4d41f8ac3fbdac3e386752ecae529c71aec4c08
Jaya Harish S <115165495+JsDawg1@users.noreply.github.com>
2024-10-04 05:26:51 -0700

    Add files via upload
    object destrurting (sorry for typos)

---
+++ object Destruring .js
@@ -0,0 +1,98 @@
+//Task1 
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+//   };
+  
+
+//   function logCarDetails({make, model, year}){
+//     console.log(`Model ${make} Model ${model} Year ${year}`)
+//   }
+
+//   logCarDetails(car)
+
+
+//Task 2
+// const book = {
+//     title: "1984",
+//     author: "George Orwell"
+//   };
+  
+//   function logBookDetails({title, author, genre="Punda ah kolapi vidum genre"}) {
+//         console.log(`Title ${title} Author of this punda book ${author} and genre ${genre}`);
+        
+//   }
+  
+//   logBookDetails(book);
+  
+
+//Task 3
+
+// const smartphone = {
+//     brand: "Apple",
+//     model: "iPhone 14",
+//     price: 999
+//   };
+  
+//   function logSmartphoneDetails({brand: phoneBrand, model: phoneModel, price}) {
+//     // Your code here
+//     console.log(`Brand name is ${phoneBrand} Phone Model is ${phoneModel} and price ${price}`);
+    
+//   }
+  
+//   logSmartphoneDetails(smartphone);
+  
+
+//Task 4
+// const movie = {
+//     title: "Inception",
+//     year: 2010,
+//     director: {
+//       name: "Christopher Nolan",
+//       age: 50
+//     }
+//   };
+  
+//   function logMovieDetails({title, year, director:{name, age}}) {
+//     // Your code here
+//     console.log(`"Title ${title} Year ${year} Director name ${name} Director age ${age}`);
+    
+//   }
+  
+//   logMovieDetails(movie);
+  
+
+
+//Task 5
+
+// const user = {
+//     username: "jdoe",
+//     email: "jdoe@example.com",
+//     isAdmin: false
+//   };
+  
+//   function logUserDetails({username, email, isAdmin}) {
+//     console.log(`ThevdiyaMone oda Username : ${username} Punda ku email vera : ${email} isAdmin ${isAdmin}`);
+    
+//   }
+  
+//   logUserDetails(user);
+  
+
+//Task 6
+
+// const playlist = {
+//     name: "Chill Vibes",
+//     songs: ["Song 1", "Song 2", "Song 3", "Song 4"]
+//   };
+  
+//   function logPlaylistDetails({songs:[song1, song2]}) {
+//     // Your code here
+//     console.log(`Song 1 ${song1} Song2 ${song2}`);
+    
+//   }
+  
+//   logPlaylistDetails(playlist);
+  
